260a2fa95ca5a0c45d184e52573a353da907c4d5
Giraldo Rosales <nitrog7@gmail.com>
2015-07-06 11:44:04 -0500

    update to 0.15.1

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ export class App {
   configureRouter(config, router){
     config.title = 'Aurelia';
     config.map([
-      { route: ['','welcome'],  moduleId: 'views/index/welcome',      nav: true, title:'Welcome' },
-      { route: 'flickr',        moduleId: 'views/index/flickr',       nav: true, title:'Flickr' },
-      { route: 'child-router',  moduleId: 'views/index/child-router', nav: true, title:'Child Router' }
+      { route: ['','welcome'],  name: 'welcome',      moduleId: 'views/index/welcome',      nav: true, title:'Welcome' },
+      { route: 'flickr',        name: 'flickr',       moduleId: 'views/index/flickr',       nav: true, title:'Flickr' },
+      { route: 'child-router',  name: 'child-router', moduleId: 'views/index/child-router', nav: true, title:'Child Router' }
     ]);
 
     this.router = router;
